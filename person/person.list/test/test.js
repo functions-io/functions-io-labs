90c2f08c89c2ae73d196f3c6243e0221ebc06b72
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-09-24 18:35:11 -0300

    refactoring

--- person/person.list/test/test.js
+++ person/person.list/test/test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const assert = require("assert");
-const moduleTest = require("../");
+const moduleTest = require("../lib");
 
 assert.strictEqual(moduleTest(2,3), 5);
 assert.strictEqual(moduleTest(2,8), 10);
